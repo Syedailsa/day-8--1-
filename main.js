c914ab6fad4c64b1f77169e6590542f12b5a52a9
Ilsa. <ilsasyeda@gmail.com>
2024-03-14 18:06:11 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,9 @@
+var colours = [
+    "red",
+    "blue",
+    "green",
+    "pink",
+    "black",
+];
+//console.log(colours{4})
+console.log(colours[4]);
---
+++ main.ts
@@ -0,0 +1,10 @@
+let colours: string[] = [
+    "red",
+    "blue",
+    "green",
+    "pink",
+    "black",
+];
+//intentional error
+//console.log(colours{4})
+console.log(colours[4]);
